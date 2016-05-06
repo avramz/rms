1574335d32718bfdb286c75d0a640316ff9b2c36
Uros <avramovic.u@gmail.com>
2016-05-07 00:09:40 +0200

    added sass

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,30 @@ gulp.task('uglify', function(){
         .pipe(plugins.uglify())
         .pipe(plugins.sourcemaps.write('./'))
         .pipe(plugins.rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy', function() {
+   return gulp.src('./src/multi-range-slider.js')
+       .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js-hint', function(){
-   return  gulp.src('multi-range-slider.js')
+   return  gulp.src('./src/multi-range-slider.js')
        .pipe(plugins.jshint())
        .pipe(plugins.jshint.reporter('default', { verbose: true }))
        .pipe(plugins.jshint.reporter('fail'));
 });
 
-gulp.task('default', ['js-hint', 'uglify']);
+gulp.task('sass', function () {
+    return gulp.src('./src/css/style.scss')
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({outputStyle: 'compressed'}))
+        .pipe(plugins.autoprefixer({ browsers: ['last 3 versions'] }))
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['js-hint', 'copy', 'uglify', 'sass']);
 
